3427404acb5484fcf613a99d05ecb9dec066c640
Sam <stschoberg@gmail.com>
2019-12-19 15:54:05 -0500

    basic header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Header.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+// const styles = {
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//    // marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+// };
+
+// const [auth, setAuth] = React.useState(true);
+// const [anchorEl, setAnchorEl] = React.useState(null);
+// const open = Boolean(anchorEl);
+
+// const handleChange = event => {
+//   setAuth(event.target.checked);
+// };
+
+// const handleMenu = event => {
+//   setAnchorEl(event.currentTarget);
+// };
+
+// const handleClose = () => {
+//   setAnchorEl(null);
+// };
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+     // marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  };
+
+class Header extends Component {
+
+
+    render() {   return (
+        <div className={styles.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={styles.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={styles.title}>
+                Photos
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+      );
+    }
+}
+
+
+export default Header;
