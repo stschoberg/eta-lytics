3536866d0369d701e15bbe7ece17e5a2076bda44
Sam <stschoberg@gmail.com>
2019-12-28 17:47:48 -0500

    updated api url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import Header from './components/Header';
-import Table from './components/Table';
 import './App.css';
 import TableComponent from './components/TableComponent';
 
--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
 import Amplify, { API } from 'aws-amplify';
 
 
@@ -52,7 +39,7 @@ const config = Amplify.configure({
       endpoints: [
           {
               name: "test123",
-              endpoint: "https://om7reelck3.execute-api.us-east-1.amazonaws.com/live/eta-lyticsGetDBLambda/"
+              endpoint: "https://dhdv449b05.execute-api.us-east-1.amazonaws.com/default/eta-lyticsGetDBLambda/"
           },
       ]
   }
